ae7f246ad3dee82c1692b47746969b678cb16d06
Isaac Benitez <ibsandoval@gmail.com>
2022-09-27 14:03:53 -0700

    Added list payout method types script

--- list-pomts.py
+++ list-pomts.py
@@ -4,15 +4,15 @@ from utilities import make_request
 import json
 import argparse 
 
-parser = argparse.ArgumentParser(description="List Payment Methods By Country")
-parser.add_argument("params", help="the url parameters between quotes, for example 'country=US&currency=USD'; params: country, currency")
+parser = argparse.ArgumentParser(description="List Payout Methods Types")
+parser.add_argument("params", help="the url parameters between quotes, for example 'payout_currency=usd&limit=2'; params: category, sender_country, sender_currency, sender_entity_type, beneficiary_country, payout_currency, beneficiary_entity_type, is_cancelable, is_location_specific, is_expirable, starting_after, ending_before, limit")
 parser.add_argument("env", help="the rapyd environment")
 parser.add_argument("-a", "--all", help="outputs all rapyd's api response", action="store_true")
 parser.add_argument("-f", "--filter", help="filters the payment method type category", dest="filter", default="", type=str)
 
 args = parser.parse_args()
 
-path = f'/v1/payment_methods/country?{args.params}'
+path = f'/v1/payouts/supported_types?{args.params}'
 
 response = make_request(env=args.env, method='get', path=path)
 
@@ -22,10 +22,11 @@ if args.all:
 
 for r in response['data']:
     if args.filter == '' or args.filter in r['category']:
-        s = f"{r['type']}, " \
+        s = f"{r['payout_method_type']}, " \
             f"{r['name']}, " \
             f"{r['category']}, " \
-            f"{r['country']}, " \
-            f"{r['currencies']}, "
+            f"{r['beneficiary_country']}, " \
+            f"payout curr: {r['payout_currencies']}, " \
+            f"sender curr: {r['sender_currencies']}"
        
         print(s)
