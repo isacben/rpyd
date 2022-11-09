7dd67baedee36dbd7dd5c8daaac4b4ffd6d12ed6
Isaac Benitez <ibsandoval@gmail.com>
2022-11-09 15:00:46 -0800

    Made output more readable

--- retrieve-multiple-pomts-required-fileds.py
+++ retrieve-multiple-pomts-required-fileds.py
@@ -15,18 +15,24 @@ pomts = f.read().splitlines()
 f.close()
 
 for pomt in pomts:
-    params = f'{pomt}/details?sender_country=MX&sender_currency=MXN&beneficiary_country=MX&payout_currency=MXN&sender_entity_type=company&beneficiary_entity_type=company&payout_amount=1000'
+    params = f'{pomt}/details?sender_country=MX&sender_currency=MXN&beneficiary_country=MX&payout_currency=MXN&sender_entity_type=company&beneficiary_entity_type=individual&payout_amount=1000'
 
     path = f'/v1/payouts/{params}'
     response = make_request(env=args.env, method='get', path=path)
 
-    print(f"{pomt},headers,,{response['data']['batch_file_header']}")
+    print(f"{pomt},headers,{response['data']['batch_file_header']}")
+    s = f"{pomt}"
     for r in response['data']['beneficiary_required_fields']:
-        print(f"{pomt},beneficiary,{r['name']},{r['type']},{r['is_required']},{r['regex']}")
+        if r['is_required'] == True:
+            s += f",beneficiary.{r['name']}"
 
     for r in response['data']['sender_required_fields']:
-        print(f"{pomt},sender,{r['name']},{r['type']},{r['is_required']},{r['regex']}")
+        if r['is_required'] == True:
+           s += f",sender.{r['name']}"
 
     if response['data']['payout_options']:
         for r in response['data']['payout_options']:
-            print(f"{pomt},options,{r['name']},{r['type']},r{['is_required']},{r['regex']}")
+            if r['is_required'] == True:
+                s += f",payout_options.{r['name']}"
+
+    print(s)
