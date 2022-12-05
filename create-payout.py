98b716b74ee18e5a226454cb4c62ce611a6ce40b
Isaac Benitez <ibsandoval@gmail.com>
2022-12-05 10:05:10 -0800

    Added random number generator to create a payout

--- create-payout.py
+++ create-payout.py
@@ -3,6 +3,7 @@
 from utilities import make_request
 import json
 import argparse 
+import random
 
 parser = argparse.ArgumentParser(description="Create Payout")
 parser.add_argument("filename", help="the json file that contains the body; see https://docs.rapyd.net/build-with-rapyd/reference/payout#create-payout")
@@ -10,10 +11,11 @@ parser.add_argument("env", help="the rapyd environment")
 parser.add_argument("-a", "--all", help="outputs all rapyd's api response", action="store_true")
 
 args = parser.parse_args()
+rand_num = random.randint(1001, 9999)
 
-confirm = input("Do you want to create this payout? (yes5972/n): ")
+confirm = input(f"Do you want to create this payout? Enter '{args.env}{rand_num}' to confirm: ")
 
-if confirm.lower() != 'yes5972':
+if confirm.lower() != args.env + str(rand_num):
     print("Payout not created")
     exit(0)
 
@@ -27,7 +29,7 @@ if args.all:
     print(json.dumps(response, indent=4))
 else:
     s = f"token: {response['data']['id']}\n" \
-        f"amount: {response['data']['original_amount']}\n" \
+        f"amount: {response['data']['amount']}\n" \
         f"payout currency: {response['data']['payout_currency']}\n" \
         f"status: {response['data']['status']}\n" \
         f"sender currency: {response['data']['sender_currency']}\n" \
